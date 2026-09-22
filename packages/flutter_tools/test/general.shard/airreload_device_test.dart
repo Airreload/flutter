16a405b682fb261646bd8e2cc2453c08fc90214f
mrnaty <yegetanaty03@gmail.com>
2026-09-22 04:01:27 +0300

    Support exact SDK identity and Android target architectures for Airreload

--- packages/flutter_tools/test/general.shard/airreload_device_test.dart
+++ packages/flutter_tools/test/general.shard/airreload_device_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter_tools/src/airreload_device.dart';
 import 'package:flutter_tools/src/application_package.dart';
 import 'package:flutter_tools/src/base/dds.dart';
@@ -15,19 +13,35 @@ import 'package:test/fake.dart';
 import '../src/common.dart';
 
 void main() {
-  testWithoutContext('supports only Android ARM64 debug attach, hot reload, and hot restart', () async {
-    final device = AirreloadDevice(logger: BufferLogger.test());
-    expect(await device.targetPlatform, TargetPlatform.android_arm64);
-    expect(device.supportsRuntimeMode(BuildMode.debug), isTrue);
-    expect(device.supportsRuntimeMode(BuildMode.profile), isFalse);
-    expect(device.supportsRuntimeMode(BuildMode.release), isFalse);
-    expect(device.supportsHotReload, isTrue);
-    expect(device.supportsHotRestart, isTrue);
-    expect(device.supportsFlutterExit, isFalse);
-    expect(device.supportsStartPaused, isFalse);
-    expect(device.portForwarder, isNull);
-    expect(device.getLogReader(), isA<NoOpDeviceLogReader>());
-  });
+  for (final platform in <TargetPlatform>[
+    TargetPlatform.android_arm,
+    TargetPlatform.android_arm64,
+    TargetPlatform.android_x64,
+  ]) {
+    testWithoutContext('reports the selected architecture $platform', () async {
+      final device = AirreloadDevice(logger: BufferLogger.test(), platform: platform);
+      expect(await device.targetPlatform, platform);
+      expect(device.supportsHotReload, isTrue);
+      expect(device.supportsHotRestart, isTrue);
+    });
+  }
+
+  testWithoutContext(
+    'supports only Android ARM64 debug attach, hot reload, and hot restart',
+    () async {
+      final device = AirreloadDevice(logger: BufferLogger.test());
+      expect(await device.targetPlatform, TargetPlatform.android_arm64);
+      expect(device.supportsRuntimeMode(BuildMode.debug), isTrue);
+      expect(device.supportsRuntimeMode(BuildMode.profile), isFalse);
+      expect(device.supportsRuntimeMode(BuildMode.release), isFalse);
+      expect(device.supportsHotReload, isTrue);
+      expect(device.supportsHotRestart, isTrue);
+      expect(device.supportsFlutterExit, isFalse);
+      expect(device.supportsStartPaused, isFalse);
+      expect(device.portForwarder, isNull);
+      expect(device.getLogReader(), isA<NoOpDeviceLogReader>());
+    },
+  );
 
   testWithoutContext('does not install, launch, uninstall, or stop an app', () async {
     final device = AirreloadDevice(logger: BufferLogger.test());
@@ -41,28 +55,23 @@ void main() {
     expect(await device.stopApp(app), isFalse);
   });
 
-  testWithoutContext('dispose waits for DDS shutdown', () async {
+  testWithoutContext('dispose shuts down DDS using the synchronous API', () async {
     final dds = PendingDartDevelopmentService();
     final device = TestAirreloadDevice(dds);
-    var disposed = false;
-    final Future<void> disposal = device.dispose().then((_) {
-      disposed = true;
-    });
-    await Future<void>.delayed(Duration.zero);
-    expect(disposed, isFalse);
-    dds.shutdownCompleter.complete();
-    await disposal;
-    expect(disposed, isTrue);
+    await device.dispose();
+    expect(dds.shutdownCalled, isTrue);
   });
 }
 
 class FakeApplicationPackage extends Fake implements ApplicationPackage {}
 
 class PendingDartDevelopmentService extends Fake implements DartDevelopmentService {
-  final shutdownCompleter = Completer<void>();
+  bool shutdownCalled = false;
 
   @override
-  Future<void> shutdown() => shutdownCompleter.future;
+  void shutdown() {
+    shutdownCalled = true;
+  }
 }
 
 class TestAirreloadDevice extends AirreloadDevice {
